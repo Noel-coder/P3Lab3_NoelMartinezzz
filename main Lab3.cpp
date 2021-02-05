3a8587e5359a6fae9188887d47880a8aebd6bc5b
Noel-coder <noelgg_23@outlook.es>
2021-02-05 13:31:39 -0600

    Menu hecho

---
+++ main Lab3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	int opcion=1;
+	while(opcion != 5) {
+		cout << "Menu" << endl
+		     << "1. Letras Unicas  " << endl
+		     << "2. Cantidad de letras en palabra " << endl
+		     << "3. Ordenamiento "<< endl
+		     << "4. Frecuencia de Letras "<<endl
+		     << "5. Salir " << endl;
+		cout << "Ingrese una opcion: " << endl;
+		cin>>opcion;
+		switch(opcion) {
+			case 1: {
+				break;
+				case 2: {
+					break;
+				}
+				case 3: {
+
+					break;
+				}
+				case 4: {
+
+					break;
+				}
+				case 5: {
+					cout << "Gracias por usar el programa :)" << endl;
+					break;
+				}
+				return 0;
+			}
+		}
+
+	}
+}
